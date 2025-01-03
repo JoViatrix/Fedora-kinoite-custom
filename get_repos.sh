e44b151bf29ccba295ee6056f7dcbbd6cdb30ad2
JoJo <lokijolijo@gmail.com>
2025-01-04 00:16:26 +0100

    Fixing more stuff

--- get_repos.sh
+++ get_repos.sh
@@ -4,5 +4,5 @@ set -eoux pipefail
 
 RELEASE="$(rpm -E %fedora)"
 
-cp /ctx/local_repos/*.repo /etc/yum.repos.d/
+cp /tmp/local_repos/*.repo /etc/yum.repos.d/
 wget https://copr.fedorainfracloud.org/coprs/rok/cdemu/repo/fedora-$RELEASE/rok-cdemu-fedora-$RELEASE.repo -O /etc/yum.repos.d/rok-cdemu.repo
