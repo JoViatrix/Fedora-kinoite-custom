896bc3c56d21b1bd997b350d7ae10d00a08ffebb
JoJo <lokijolijo@gmail.com>
2025-01-03 23:48:43 +0100

    test

--- build.sh
+++ build.sh
@@ -13,21 +13,12 @@ RELEASE="$(rpm -E %fedora)"
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-<<<<<<< HEAD
 # rpm-ostree install screen
-=======
-rpm-ostree install screen
->>>>>>> cf59fdf95fac4e3193e68f8a79b2b6497549200c
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
 
 #### Example for enabling a System Unit File
-<<<<<<< HEAD
 # systemctl enable podman.socket
 
-rpm-ostree install kernel-devel winehq-staging cdemu-client cdemu-daemon gcdemu snapd
-=======
-
-systemctl enable podman.socket
->>>>>>> cf59fdf95fac4e3193e68f8a79b2b6497549200c
+rpm-ostree install kernel-devel winehq-staging cdemu-client cdemu-daemon gcdemu snapd virt-manager
