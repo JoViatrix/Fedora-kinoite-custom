63c3b460750728ef6ad6947f3e4c7aca34530f04
JoJo <lokijolijo@gmail.com>
2025-01-04 13:37:49 +0100

    figuring things out

--- build.sh
+++ build.sh
@@ -12,19 +12,13 @@ RELEASE="$(rpm -E %fedora)"
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-# rpm-ostree install screen
-
-# this would install a package from rpmfusion
-# rpm-ostree install vlc
-
 #### Example for enabling a System Unit File
 # systemctl enable podman.socket
 /tmp/get_repos.sh
 dnf5 install -y kernel-devel-matched
 
-mkdir -p /opt/wine-staging/bin
-dnf5 install -y winehq-staging snapd # nextcloud-client-dolphin
+# Install wine staging from winehq.org
+/tmp/winehq.sh
 
 # Install virt-manager and qemu-kvm
 dnf5 install -y virt-manager virt-install libvirt-daemon-kvm libvirt-daemon-config-network
@@ -42,3 +36,9 @@ dnf install -y xone lpf-xone-firmware
 lpf approve xone-firmware
 lpf build xone-firmware
 lpf install -y xone-firmware
+
+# Install snapd
+
+dnf5 install -y snapd
+
+dnf5 install -y nextcloud-client-dolphin
