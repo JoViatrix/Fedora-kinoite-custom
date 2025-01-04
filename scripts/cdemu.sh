0a953eadc8f8c2d2893b8c8a9c2128bd83e26665
JoJo <lokijolijo@gmail.com>
2025-01-04 23:38:02 +0100

    more testing

--- scripts/cdemu.sh
+++ scripts/cdemu.sh
@@ -6,7 +6,7 @@ echo "Installing cdemu"
 
 dnf5 -y copr enable rok/cdemu
 
-dnf5 install -y cdemu-client cdemu-daemon gcdemu
+rpm-ostree install --idempotent cdemu-client cdemu-daemon gcdemu
 
 cat >/etc/modules-load.d/vhda.conf <<EOF
 vhda
--- scripts/xpadneo.sh
+++ scripts/xpadneo.sh
@@ -6,7 +6,7 @@ echo "Installing xpadneo"
 
 dnf5 -y copr enable atim/xpadneo
 
-dnf5 install -y xpadneo
+rpm-ostree install --idempotent xpadneo
 
 cat >/etc/modules-load.d/xpadneo.conf <<EOF
 hid_xpadneo
