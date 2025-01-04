baadee2c920ce67bb0c114a07685766acd767def
JoJo <lokijolijo@gmail.com>
2025-01-04 21:56:28 +0100

    fixing small mistake

--- scripts/build.sh
+++ scripts/build.sh
@@ -64,12 +64,11 @@ dnf5 install -y nextcloud-client-dolphin
 dnf5 install -y steam mangohud gamescope
 
 # Install zerotier-one
-
+dnf5 install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$RELEASE.noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$RELEASE.noarch.rpm
 dnf5 install -y zerotier-one
 
 # Install additonal codecs multimedia group
 
-dnf5 install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$RELEASE.noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$RELEASE.noarch.rpm
 dnf5 group install -y multimedia
 dnf5 install -y libavcodec-freeworld
 dnf5 remove -y rpmfusion-free-release rpmfusion-nonfree-release
