72f7967adfc01c3167b3848fb4e49a0c36828c75
JoJo <lokijolijo@gmail.com>
2025-01-04 09:41:22 +0100

    Test

--- get_repos.sh
+++ get_repos.sh
@@ -3,6 +3,7 @@
 set -eoux pipefail
 
 RELEASE="$(rpm -E %fedora)"
-
+ls /tmp/
+ls /tmp/local_repos/
 cp /tmp/local_repos/*.repo /etc/yum.repos.d/
 wget https://copr.fedorainfracloud.org/coprs/rok/cdemu/repo/fedora-$RELEASE/rok-cdemu-fedora-$RELEASE.repo -O /etc/yum.repos.d/rok-cdemu.repo
