8eb43d8c505b810b5d7d84daf68ccb7254c63167
JoJo <lokijolijo@gmail.com>
2025-01-05 10:48:13 +0100

    More fixing

--- scripts/cdemu.sh
+++ scripts/cdemu.sh
@@ -8,7 +8,10 @@ dnf5 -y copr enable rok/cdemu
 
 dnf5 install -y cdemu-client cdemu-daemon gcdemu
 
-akmods --force --kernels "$(rpm -q kernel)" --kmod vhba
+KERNEL_SUFFIX=""
+KERNEL="$(rpm -qa | grep -P 'kernel-(|'"$KERNEL_SUFFIX"'-)(\d+\.\d+\.\d+)' | sed -E 's/kernel-(|'"$KERNEL_SUFFIX"'-)//')"
+
+akmods --force --kernels "$KERNEL" --kmod vhba
 
 cat >/etc/modules-load.d/vhda.conf <<EOF
 vhda
--- scripts/xpadneo.sh
+++ scripts/xpadneo.sh
@@ -8,7 +8,10 @@ dnf5 -y copr enable atim/xpadneo
 
 dnf5 install -y xpadneo
 
-akmods --force --kernels "$(rpm -q kernel)" --kmod xpadneo
+KERNEL_SUFFIX=""
+KERNEL="$(rpm -qa | grep -P 'kernel-(|'"$KERNEL_SUFFIX"'-)(\d+\.\d+\.\d+)' | sed -E 's/kernel-(|'"$KERNEL_SUFFIX"'-)//')"
+
+akmods --force --kernels "$KERNEL" --kmod xpadneo
 
 cat >/etc/modules-load.d/xpadneo.conf <<EOF
 hid_xpadneo
