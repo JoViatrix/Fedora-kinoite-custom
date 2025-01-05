ade2a53da3c3d7aa8f9b0438dd46022bcf90c7c7
JoJo <lokijolijo@gmail.com>
2025-01-05 21:29:32 +0100

    fix missing -y parameter and add script for downloading xbox dongle firmware

--- scripts/snapd.sh
+++ scripts/snapd.sh
@@ -7,7 +7,7 @@ echo "Installing snapd"
 dnf5 install -y snapd
 systemctl enable snapd
 
-sudo ln -s /var/lib/snapd/snap /snap
+ln -s /var/lib/snapd/snap /snap
 cat >/usr/lib/tmpfiles.d/snapd.conf <<EOF
 L /snap - - - - /var/lib/snapd/snap
 EOF
--- scripts/xone.sh
+++ scripts/xone.sh
@@ -15,10 +15,53 @@ skopeo copy docker://ghcr.io/ublue-os/akmods:"${KERNEL_FLAVOR}"-"${RELEASE}"-"${
 AKMODS_TARGZ=$(jq -r '.layers[].digest' < /tmp/akmods/manifest.json | cut -d : -f 2)
 tar -xvzf /tmp/akmods/"$AKMODS_TARGZ" -C /tmp/
 
-dnf5 install -y xone lpf-xone-firmware /tmp/rpms/kmods/*xone*.rpm
+dnf5 install -y xone /tmp/rpms/kmods/*xone*.rpm
 
-#lpf approve xone-firmware
-#lpf build xone-firmware
-#lpf install -y xone-firmware
+cat >/usr/lib/tmpfiles.d/xone_firmware.conf <<EOF
+L /lib/firmware/xow_dongle.bin - - - - /var/lib/firmware/xow_dongle.bin
+EOF
+
+cat >/usr/local/bin/xone-get-firmware.sh <<EOF
+#!/usr/bin/env sh
+
+# This script is based on https://github.com/medusalix/xone/blob/master/install.sh. Thx to all contributors of xone!
+
+set -eu
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo 'This script must be run as root!' >&2
+    exit 1
+fi
+
+if ! [ -x "$(command -v curl)" ]; then
+    echo 'This script requires curl!' >&2
+    exit 1
+fi
+
+if ! [ -x "$(command -v cabextract)" ]; then
+    echo 'This script requires cabextract!' >&2
+    exit 1
+fi
+
+if [ "${1:-}" != --skip-disclaimer ]; then
+    echo "The firmware for the wireless dongle is subject to Microsoft's Terms of Use:"
+    echo 'https://www.microsoft.com/en-us/legal/terms-of-use'
+    echo
+    echo 'Press enter to continue!'
+    read -r _
+fi
+
+driver_url='http://download.windowsupdate.com/c/msdownload/update/driver/drvs/2017/07/1cd6a87c-623f-4407-a52d-c31be49e925c_e19f60808bdcbfbd3c3df6be3e71ffc52e43261e.cab'
+firmware_hash='48084d9fa53b9bb04358f3bb127b7495dc8f7bb0b3ca1437bd24ef2b6eabdf66'
+
+curl -L -o driver.cab "$driver_url"
+cabextract -F FW_ACC_00U.bin driver.cab
+echo "$firmware_hash" FW_ACC_00U.bin | sha256sum -c
+mkdir -p /var/lib/firmware
+mv FW_ACC_00U.bin /var/lib/firmware/xow_dongle.bin
+rm driver.cab
+EOF
+
+chmod +x /usr/local/bin/xone-get-firmware.sh
 
 dnf5 copr remove sentry/xone
