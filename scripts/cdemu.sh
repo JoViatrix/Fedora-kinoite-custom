e415acf182bcb589b35daa16087867097c1c0a00
JoJo <lokijolijo@gmail.com>
2025-01-05 13:45:50 +0100

    add logs

--- scripts/cdemu.sh
+++ scripts/cdemu.sh
@@ -13,6 +13,9 @@ KERNEL="$(rpm -qa | grep -P 'kernel-(|'"$KERNEL_SUFFIX"'-)(\d+\.\d+\.\d+)' | sed
 
 akmods --force --kernels "$KERNEL" --kmod vhba
 
+modinfo /usr/lib/modules/${KERNEL}/extra/xpadneo/vhda.ko.xz > /dev/null \
+|| (find /var/cache/akmods/vhda/ -name \*.log -print -exec cat {} \; && exit 1)
+
 cat >/etc/modules-load.d/vhda.conf <<EOF
 vhda
 EOF
--- scripts/xpadneo.sh
+++ scripts/xpadneo.sh
@@ -6,12 +6,15 @@ echo "Installing xpadneo"
 
 dnf5 -y copr enable atim/xpadneo
 
-dnf5 install -y xpadneo kmod-xpadneo
+dnf5 install -y xpadneo
 
 KERNEL_SUFFIX=""
 KERNEL="$(rpm -qa | grep -P 'kernel-(|'"$KERNEL_SUFFIX"'-)(\d+\.\d+\.\d+)' | sed -E 's/kernel-(|'"$KERNEL_SUFFIX"'-)//')"
 
 akmods --force --kernels "$KERNEL" --kmod xpadneo
+
+modinfo /usr/lib/modules/${KERNEL}/extra/xpadneo/hid-xpadneo.ko.xz > /dev/null \
+|| (find /var/cache/akmods/xpadneo/ -name \*.log -print -exec cat {} \; && exit 1)
 
 cat >/etc/modules-load.d/xpadneo.conf <<EOF
 hid_xpadneo
