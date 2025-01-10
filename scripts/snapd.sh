913fda70b74131915bcd845483c83d2e7944974e
JoViatrix <62498297+JoViatrix@users.noreply.github.com>
2025-01-10 09:42:37 +0100

    Update snapd.sh

--- scripts/snapd.sh
+++ scripts/snapd.sh
@@ -5,7 +5,7 @@ set -ouex pipefail
 echo "Installing snapd"
 
 dnf5 install -y snapd
-/usr/bin/systemctl enable snapd
+systemctl enable snapd
 
 # ln -s /var/lib/snapd/snap /snap
 # cat >/usr/lib/tmpfiles.d/snapd.conf <<EOF
