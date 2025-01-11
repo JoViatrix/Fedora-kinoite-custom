54c22e6ccd0a7aa8f50f309a457f03e9f65ffab1
JoViatrix <62498297+JoViatrix@users.noreply.github.com>
2025-01-11 15:57:30 +0100

    Update mesa.sh

--- scripts/mesa.sh
+++ scripts/mesa.sh
@@ -4,4 +4,8 @@ set -ouex pipefail
 
 echo "Updating mesa"
 
-dnf5 update -y mesa-* vulkan-* --repo=updates
+dnf5 -y copr enable che/mesa
+
+dnf5 update -y --repo=copr:copr.fedorainfracloud.org:che:mesa,copr:copr.fedorainfracloud.org:che:mesa:ml
+
+dnf5 copr remove che/mesa
