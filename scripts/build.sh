4ba6a46558b8f1136255fcd3b293102fead557ff
JoViatrix <62498297+JoViatrix@users.noreply.github.com>
2025-01-12 11:57:12 +0100

    adding missing group

--- scripts/build.sh
+++ scripts/build.sh
@@ -35,6 +35,7 @@ dnf5 remove -y firefox firefox-langpacks kate
 dnf5 install -y dosbox
 
 # Install virt-manager and qemu-kvm
+groupadd --system kvm
 dnf5 install -y virt-manager virt-install libvirt-daemon-kvm libvirt-daemon-config-network
 
 # Install cdemu module and gui
