e7c29386de26f663f0df6e96c758c46fed7256e5
JoJo <lokijolijo@gmail.com>
2025-01-31 11:35:51 +0100

    fix bitwarden install

--- scripts/bitwarden.sh
+++ scripts/bitwarden.sh
@@ -6,4 +6,11 @@ echo "Installing bitwarden"
 
 wget "https://vault.bitwarden.com/download/?app=desktop&platform=linux&variant=rpm" -O /tmp/bitwarden.rpm
 
+
+mkdir -p /var/opt
 dnf5 install -y /tmp/bitwarden.rpm
+mv /var/opt/Bitwarden /usr/lib/opt/Bitwarden
+
+cat >/usr/lib/tmpfiles.d/bitwarden.conf <<EOF
+L /opt/Bitwarden - - - - /usr/lib/opt/Bitwarden
+EOF
