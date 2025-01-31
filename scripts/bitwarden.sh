3cd6c33a3424aa9375368058108b19123fd60db5
JoJo <lokijolijo@gmail.com>
2025-01-31 12:04:15 +0100

    better integration for bitwarden

--- scripts/bitwarden.sh
+++ scripts/bitwarden.sh
@@ -14,3 +14,23 @@ mv /var/opt/Bitwarden /usr/lib/opt/Bitwarden
 cat >/usr/lib/tmpfiles.d/bitwarden.conf <<EOF
 L /opt/Bitwarden - - - - /usr/lib/opt/Bitwarden
 EOF
+
+echo '<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE policyconfig PUBLIC
+ "-//freedesktop//DTD PolicyKit Policy Configuration 1.0//EN"
+ "http://www.freedesktop.org/standards/PolicyKit/1.0/policyconfig.dtd">
+
+<policyconfig>
+    <action id="com.bitwarden.Bitwarden.unlock">
+      <description>Unlock Bitwarden</description>
+      <message>Authenticate to unlock Bitwarden</message>
+      <defaults>
+        <allow_any>no</allow_any>
+        <allow_inactive>no</allow_inactive>
+        <allow_active>auth_self</allow_active>
+      </defaults>
+    </action>
+</policyconfig>' > /usr/share/polkit-1/actions/com.bitwarden.Bitwarden.policy
+
+chown root:root /usr/share/polkit-1/actions/com.bitwarden.Bitwarden.policy
+chcon system_u:object_r:usr_t:s0 /usr/share/polkit-1/actions/com.bitwarden.Bitwarden.policy
